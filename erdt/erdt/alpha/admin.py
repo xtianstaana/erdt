5fc85e6af0cb29cd9f82ff2429d8e1f5bb4ba79c
Christian Sta.Ana <xtian.staana@gmail.com>
2014-07-16 21:13:54 +0800

    Initial custom admin templates added

---
+++ erdt/erdt/alpha/admin.py
@@ -0,0 +1,36 @@
+from django.contrib.admin import AdminSite
+from functools import update_wrapper
+from django.http import Http404, HttpResponseRedirect
+from django.contrib.admin import ModelAdmin, actions
+from django.contrib.admin.forms import AdminAuthenticationForm
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.models import User
+from django.contrib.contenttypes import views as contenttype_views
+from django.views.decorators.csrf import csrf_protect
+from django.db.models.base import ModelBase
+from django.core.exceptions import ImproperlyConfigured
+from django.core.urlresolvers import reverse, NoReverseMatch
+from django.template.response import TemplateResponse
+from django.utils.safestring import mark_safe
+from django.utils.text import capfirst
+from django.utils.translation import ugettext as _
+from django.views.decorators.cache import never_cache
+from django.conf import settings
+
+class ERDTAdminSite(AdminSite):
+    @never_cache
+    def index(self, request, extra_context=None):
+        erdtIndexTempRes = super(ERDTAdminSite, self).index(request)
+
+        erdtIndexTempRes.context_data['title'] = 'Profile'
+
+        # get current user data
+        currentUser = request.user
+        erdtIndexTempRes.context_data['current_user'] = currentUser
+
+        return erdtIndexTempRes
+
+admin_site = ERDTAdminSite()
+
+admin_site.register(User)
+
